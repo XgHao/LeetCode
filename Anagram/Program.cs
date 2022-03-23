5d1483b919312897c5952f4fe3e8e8f826178a8c
XgHao <zxh957553851@gmail.com>
2022-03-23 15:16:32 +0800

    MajorityElement and IsAnagram

---
+++ Anagram/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    var sStr = Console.ReadLine();
+    var tStr = Console.ReadLine();
+    Console.WriteLine(IsAnagram(sStr.ToCharArray(), tStr.ToCharArray()));
+}
+
+bool IsAnagram(char[] sArr, char[] tArr)
+{
+    if (sArr.Length != tArr.Length)
+    {
+        return false;
+    }
+
+    var sCount = new int[26];
+    var tCount = new int[26];
+
+    var len = sArr.Length;
+    for (int i = 0; i < len; i++)
+    {
+        var sIndex = sArr[i] - 'a';
+        sCount[sIndex]++;
+        var tIndex = tArr[i] - 'a';
+        tCount[tIndex]++;
+    }
+
+    for (int i = 0; i < sCount.Length; i++)
+    {
+        if (sCount[i] != tCount[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ Majority_Element/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    var input = Console.ReadLine();
+    var arr = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(e => Convert.ToInt32(e)).ToArray();
+    Console.WriteLine(MajorityElement(arr));
+    Console.WriteLine(MajorityElementV2(arr));
+}
+
+int MajorityElement(int[] arr)
+{
+    if (!(arr?.Length > 0))
+    {
+        return 0;
+    }
+    var stack = new Stack<int>();
+
+    foreach (var num in arr)
+    {
+        // 没有元素Push
+        if (!stack.TryPeek(out var popNum))
+        {
+            stack.Push(num);
+        }
+        // 有元素，且值相同Push
+        else if (popNum == num)
+        {
+            stack.Push(num);
+        }
+        // 有元素，值不同Pop
+        else
+        {
+            stack.Pop();
+        }
+    }
+
+    return stack.Pop();
+}
+
+int MajorityElementV2(int[] arr)
+{
+    if (!(arr?.Length > 0))
+    {
+        return 0;
+    }
+
+    var keyNum = 0;
+    var keyNumCount = 0;
+
+    foreach (var num in arr)
+    {
+        // 没有元素Push
+        if (keyNumCount == 0)
+        {
+            keyNum = num;
+            keyNumCount++;
+        }
+        else if (keyNum == num)
+        {
+            keyNumCount++;
+        }
+        else
+        {
+            keyNumCount--;
+        }
+    }
+
+    return keyNum;
+}
