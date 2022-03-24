96e52b173c8f42d9e3c28eebef3be12232dd97dd
XgHao <zxh957553851@gmail.com>
2022-03-24 16:07:21 +0800

    IsPowerOfTwo and MergeSortArray

---
+++ IsPowerOfTwo/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    var num = Console.ReadLine();
+    Console.WriteLine(IsPowerOfTwo(long.Parse(num)));
+    Console.WriteLine(IsPowerOfTwoV2(long.Parse(num)));
+}
+
+bool IsPowerOfTwo(long num)
+{
+    if (num <= 1)
+    {
+        return true;
+    }
+
+    if (num % 2 != 0)
+    {
+        return false;
+    }
+
+    return IsPowerOfTwo(num / 2);
+}
+
+bool IsPowerOfTwoV2(long num)
+{
+    if (num <= 1)
+    {
+        return true;
+    }
+
+    return (num & (num - 1)) == 0;
+}
---
+++ MergeSortArray/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    var input = Console.ReadLine();
+    var aArr = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(e => Convert.ToInt32(e)).ToArray();
+    input = Console.ReadLine();
+    var bArr = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(e => Convert.ToInt32(e)).ToArray();
+    Merge(aArr, bArr).ToList().ForEach(Console.Write);
+}
+
+int[] Merge(int[] aArr, int[] bArr)
+{
+    // aArr及bArr以为排序数组
+    var resArr = new int[aArr.Length + bArr.Length];
+
+    int i = 0, j = 0, k = 0;
+    while (i < aArr.Length && j < bArr.Length)
+    {
+        if (aArr[i] < bArr[j])
+        {
+            resArr[k++] = aArr[i++];
+        }
+        else
+        {
+            resArr[k++] = bArr[j++];
+        }
+    }
+
+    // 放入剩下的
+    while (i < aArr.Length)
+    {
+        resArr[k++]=aArr[i++];
+    }
+
+    // 放入剩下的
+    while (j < bArr.Length)
+    {
+        resArr[k++] = bArr[j++];
+    }
+
+    return resArr;
+}
