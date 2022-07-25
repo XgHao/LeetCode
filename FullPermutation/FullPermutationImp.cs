e96277114bb5142fe58e14d07b09a475463df035
XgHao <zxh957553851@gmail.com>
2022-07-25 13:49:12 +0800

    FullPermutation

---
+++ FullPermutation/FullPermutationImp.cs
@@ -0,0 +1,33 @@
+﻿namespace FullPermutation;
+
+public static class FullPermutationImp
+{
+    private static int _count = 0;
+
+    /// <summary>
+    /// 全排列.
+    /// </summary>
+    /// <param name="arr">要排列的数组.</param>
+    /// <param name="beginIndex">排列的开始索引.</param>
+    public static void FullPermutation(this int[] arr, int beginIndex = 0)
+    {
+        // 只剩一个数字时，排列结束
+        if (beginIndex + 1 == arr.Length)
+        {
+            Console.Write($"第{(++_count),-3}种排列：[");
+            arr.ToList().ForEach(Console.Write);
+            Console.WriteLine("]");
+            return;
+        }
+
+        // 每个数字都放到头部.
+        for (int index = beginIndex; index < arr.Length; index++)
+        {
+            (arr[index], arr[beginIndex]) = (arr[beginIndex], arr[index]);
+
+            FullPermutation(arr, beginIndex + 1);
+
+            (arr[index], arr[beginIndex]) = (arr[beginIndex], arr[index]);
+        }
+    }
+}
---
+++ FullPermutation/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FullPermutation;
+
+while (true)
+{
+    Console.WriteLine("输入要全排列的数字，通过空格分开，回车确认");
+    Console.ReadLine()
+        ?.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Where(e => int.TryParse(e, out _))
+        .Select(int.Parse)
+        .ToArray()
+        .FullPermutation();
+}
+
